153156b373e949ec318aa4c5be4bf8aefa03e41a
Ashay Vipinkumar <vipinkum@usc.edu>
2017-04-08 17:48:30 -0700

    jsps

--- CSCI201Factory/src/actions/Action.java
+++
@@ -1,9 +0,0 @@
-package actions;
-
-import com.google.gson.JsonObject;
-
-import client.Factory;
-
-public abstract class Action {
-	public abstract void execute(Factory factory, JsonObject msg);
-}
--- CSCI201Factory/src/messages/Message.java
+++
@@ -1,8 +0,0 @@
-package messages;
-
-import java.io.Serializable;
-
-public class Message implements Serializable {
-	private static final long serialVersionUID = 1L;
-	public String action;
-}

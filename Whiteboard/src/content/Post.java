1ea959597f35f8153b047136604539762057f7c9
grant <grantsprice@gmail.com>
2017-04-09 15:53:17 -0700

    Added ParentID to post class, improved appearance of landing page

--- Whiteboard/src/content/Post.java
+++ Whiteboard/src/content/Post.java
@@ -7,12 +7,22 @@ public class Post extends Content{
 	private String title;
 	private String body;
 	private ArrayList<Post> replies;
+	private int parentID;
 
 	public Post(String contentID, String userID, String classID, Date time, String title, String body) {
 		super(contentID, userID, classID, time);
 		this.setTitle(title);
 		this.setBody(body);
 		this.setReplies(new ArrayList<Post>());
+		this.parentID = 0;
+	}
+	
+	public Post(String contentID, String userID, String classID, Date time, String title, String body, int parentID) {
+		super(contentID, userID, classID, time);
+		this.setTitle(title);
+		this.setBody(body);
+		this.setReplies(new ArrayList<Post>());
+		this.parentID = parentID;
 	}
 
 	public ArrayList<Post> getReplies() {
@@ -42,5 +52,13 @@ public class Post extends Content{
 	public void addReply(Post reply)
 	{
 		replies.add(reply);
+	}
+
+	public int getParentID() {
+		return parentID;
+	}
+
+	public void setParentID(int parentID) {
+		this.parentID = parentID;
 	}
 }

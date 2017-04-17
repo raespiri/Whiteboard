3875dc0195260689cfc2706241cb7e82a6c4ae6b
grant <grantsprice@gmail.com>
2017-04-16 22:50:37 -0700

    moderation done

--- Whiteboard/WebContent/js/mod.js
+++ Whiteboard/WebContent/js/mod.js
@@ -15,3 +15,18 @@ function deletePost(postID)
 	}
 	req.send(null);
 }
+
+function deleteDocument(documentID)
+{
+	var url = "../ModServlet?type=doc&id="+documentID
+	// create AJAX request
+	var req = new XMLHttpRequest();
+	req.open("GET", url, true);
+	
+	req.onreadystatechange = function () {
+		if(req.readyState == 4 && req.status == 200) { 
+			location.reload(true);
+		}
+	}
+	req.send(null);
+}
